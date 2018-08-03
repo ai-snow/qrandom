64c2a953884d2c6df88e1743e36209e2be9a8801
Sam Wehner <31450492+ai-snow@users.noreply.github.com>
2018-08-03 09:01:30 -0500

    Test reset

--- test/test.py
+++ test/test.py
@@ -4,9 +4,17 @@ from qrandom.qrandom import QRandom
 
 q = QRandom(Random())
 
-first = q.q_first
-second = q.q_second
-first = q.q_first
+print('First run...')
+print(q.q_first)
+print(q.q_first)
 
+q.reset()
+
+print('Second run...')
+print(q.q_first)
+print(q.q_second)
+print(q.q_first)
+
+print('Stats...')
 print(q.quantiles)
 print(q.buffer)
