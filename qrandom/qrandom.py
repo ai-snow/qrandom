88db9f5b938228e6c4b8d05a3890b7d477c99cce
Sam Wehner <samcwehner@gmail.com>
2018-08-03 06:25:10 -0500

    __getattribute__ implementation

--- qrandom/qrandom.py
+++ qrandom/qrandom.py
@@ -1,13 +1,26 @@
+from collections import deque
 
 
 class QRandom(object):
 
     def __init__(self, random):
         self.random = random
+        self.quantiles = {}
         self.buffer = []
+        self.buffer_index = 0
 
     def __quantile(self):
         pass
 
     def __getattribute__(self, attr):
-        print(attr)
+        if attr.startswith('q_'):
+            quantile_name = attr[2:]
+            quantile = self.random.random()
+            self.buffer.append(quantile)
+            if quantile_name not in self.quantiles:
+                self.quantiles[quantile_name] = deque()
+            self.quantiles[quantile_name].appendleft(self.buffer_index)
+            self.buffer_index += 1
+            return quantile
+        else:
+            return super(QRandom, self).__getattribute__(attr)
--- test/test.py
+++ test/test.py
@@ -5,3 +5,7 @@ from qrandom.qrandom import QRandom
 q = QRandom(Random())
 
 first = q.q_first
+second = q.q_second
+first = q.q_first
+
+print(q.quantiles)
