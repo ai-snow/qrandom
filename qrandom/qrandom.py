d09bfc736e8a728d6e52b521d961a5861d4c1781
Sam Wehner <31450492+ai-snow@users.noreply.github.com>
2018-08-03 08:06:01 -0500

    Better __getattribute__ implementation

--- qrandom/qrandom.py
+++ qrandom/qrandom.py
@@ -1,26 +1,46 @@
 from collections import deque
 
 
+from quantile import Quantile
+
+
 class QRandom(object):
 
-    def __init__(self, random):
+    def __init__(self, random, seed):
         self.random = random
+        self.seed = seed
+        self.random.seed(seed)
         self.quantiles = {}
         self.buffer = []
-        self.buffer_index = 0
 
-    def __quantile(self):
-        pass
-
+    def __fill_buffer(self, index):
+        while len(self.buffer) <= index:
+            self.buffer.append(self.random.random())
+            
+        return self.buffer[index]
+        
     def __getattribute__(self, attr):
         if attr.startswith('q_'):
             quantile_name = attr[2:]
-            quantile = self.random.random()
-            self.buffer.append(quantile)
             if quantile_name not in self.quantiles:
-                self.quantiles[quantile_name] = deque()
-            self.quantiles[quantile_name].appendleft(self.buffer_index)
-            self.buffer_index += 1
-            return quantile
+                self.quantiles[quantile_name] = Quantile()
+            quantile = self.quantiles[quantile_name]
+            q = None
+            if len(quantile.waiting):
+                index = quantile.waiting.pop()
+                q = self.__fill_buffer(index)
+                quantile.used.appendleft(index)
+            else:
+                index = len(self.buffer)
+                q = self.__fill_buffer(index)
+                quantile.used.appendleft(index)
+                
+            return q
         else:
             return super(QRandom, self).__getattribute__(attr)
+    
+    def reset(self):
+        self.random.seed(self.seed)
+        
+        for _, quantile in self.quantiles:
+            quantile.prime()
